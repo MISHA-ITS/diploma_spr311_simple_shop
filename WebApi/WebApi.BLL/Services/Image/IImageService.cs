43b85ffcf7b23ff3e6604c50f6c0cf9bb0d8bd47
MISHA-ITS <mybox018@gmail.com>
2025-11-16 17:38:51 +0200

    Add Image Service

---
+++ WebApi/WebApi.BLL/Services/Image/IImageService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace WebApi.BLL.Services.Image;
+
+public interface IImageService
+{
+    public interface IImageService
+    {
+        Task<string?> SaveImageAsync(IFormFile image, string filePath);
+        void DeleteImage(string directory);
+
+        Task<List<string>> SaveProductImagesAsync(List<IFormFile> images, string path);
+    }
+}
---
+++ WebApi/WebApi.BLL/Services/Image/ImageService.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Webp;
+using SixLabors.ImageSharp.Processing;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApi.BLL.Services.Image;
+
+public class ImageService(IConfiguration configuration) : IImageService
+{
+    public void DeleteImage(string filePath)
+    {
+        if (string.IsNullOrEmpty(Settings.ImagesPath))
+        {
+            return;
+        }
+
+        string workPath = Path.Combine(Settings.ImagesPath, filePath);
+
+        if (File.Exists(workPath))
+        {
+            File.Delete(workPath);
+        }
+    }
+
+    public async Task<string?> SaveImageAsync(IFormFile image, string directory)
+    {
+        if (string.IsNullOrEmpty(Settings.ImagesPath))
+        {
+            return null;
+        }
+
+        var types = image.ContentType.Split('/');
+
+        if (types[0] == "image")
+        {
+            string imageName = $"{Guid.NewGuid()}.{types[1]}";
+            string workPath = Path.Combine(Settings.ImagesPath, directory);
+            string filePath = Path.Combine(workPath, imageName);
+
+            if (!Directory.Exists(workPath))
+            {
+                Directory.CreateDirectory(workPath);
+            }
+
+            using (var stream = File.Create(filePath))
+            {
+                await image.CopyToAsync(stream);
+            }
+
+            return imageName;
+        }
+
+        return null;
+    }
+
+    public async Task<List<string>> SaveProductImagesAsync(List<IFormFile> images, string path)
+    {
+        List<string> imagesName = new List<string>();
+
+        foreach (var image in images)
+        {
+            string? imageName = await SaveImageAsync(image, path);
+            if (imageName != null)
+            {
+                imagesName.Add(imageName);
+            }
+        }
+
+        return imagesName;
+    }
+}
