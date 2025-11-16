0c71f540042546797fafdaf375323d89215f0d0a
MISHA-ITS <mybox018@gmail.com>
2025-11-16 11:24:49 +0200

    Add ServiceResponse

--- WebApi/WebApi.DAL/Repositories/GenericRepository.cs
+++ WebApi/WebApi.DAL/Repositories/GenericRepository.cs
@@ -3,42 +3,35 @@ using WebApi.DAL.Entities;
 
 namespace WebApi.DAL.Repositories;
 
-public class GenericRepository<TEntity, TId> : IGenericRepository<TEntity, TId>
+public class GenericRepository<TEntity, TId>(AppDbContext context) : IGenericRepository<TEntity, TId>
     where TEntity : class, IBaseEntity<TId>
     where TId : notnull
 {
-    private readonly AppDbContext _context;
-
-    public GenericRepository(AppDbContext context)
-    {
-        _context = context;
-    }
-
     public virtual async Task<bool> CreateAsync(TEntity entity)
     {
         entity.CreateDate = DateTime.UtcNow;
         entity.UpdateDate = DateTime.UtcNow;
-        object value = await _context.Set<TEntity>().AddAsync(entity);
-        var result = await _context.SaveChangesAsync();
+        object value = await context.Set<TEntity>().AddAsync(entity);
+        var result = await context.SaveChangesAsync();
         return result != 0;
     }
 
     public virtual async Task<bool> DeleteAsync(TEntity entity)
     {
         entity.UpdateDate = DateTime.UtcNow;
-        object value = _context.Set<TEntity>().Remove(entity);
-        var result = await _context.SaveChangesAsync();
+        object value = context.Set<TEntity>().Remove(entity);
+        var result = await context.SaveChangesAsync();
         return result != 0;
     }
 
     public virtual IQueryable<TEntity> GetAll()
     {
-        return _context.Set<TEntity>();
+        return context.Set<TEntity>();
     }
 
     public virtual async Task<TEntity?> GetByIdAsync(TId id)
     {
-        var result = await _context.Set<TEntity>()
+        var result = await context.Set<TEntity>()
             .FirstOrDefaultAsync(e => e.Id.Equals(id));
         return result;
     }
@@ -46,8 +39,8 @@ public class GenericRepository<TEntity, TId> : IGenericRepository<TEntity, TId>
     public virtual async Task<bool> UpdateAsync(TEntity entity)
     {
         entity.UpdateDate = DateTime.UtcNow;
-        object value = _context.Set<TEntity>().Update(entity);
-        var result = await _context.SaveChangesAsync();
+        object value = context.Set<TEntity>().Update(entity);
+        var result = await context.SaveChangesAsync();
         return result != 0;
     }
 }
