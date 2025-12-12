1083c24ff56e6764c71d26dc538755658bcf9163
maksym <maksdudiya25@gmail.com>
2025-12-12 21:14:47 +0100

    Add Product and ProductImage entities

---
+++ WebApi/WebApi.DAL/Entities/ProductEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.DAL.Entities
+{
+    public class ProductEntity : BaseEntity<long>
+    {
+        [Required]
+        [MaxLength(255)]
+        public required string Name { get; set; }
+        public string Slug { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+        public ICollection<CategoryEntity> Categories { get; set; } = [];
+        public ICollection<ProductImageEntity> Images { get; set; } = [];
+    }
+}
