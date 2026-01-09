df0b1aa43cfb7de0dc8ec7fd16ce0a7a7c222b4e
novakvova <novakvova@gmail.com>
2026-01-09 21:05:47 +0200

    Add Categories is good

--- WebApi/WebApi.BLL/DTOs/Category/CreateCategoryDTO.cs
+++ WebApi/WebApi.BLL/DTOs/Category/CreateCategoryDTO.cs
@@ -6,6 +6,7 @@ namespace WebApi.BLL.DTOs.Category
     {
         public string Name { get; set; } = string.Empty;
         public string Slug { get; set; } = string.Empty;
+        public int? ParentId { get; init; }
         public IFormFile? Image { get; set; }
     }
 }
