6f87f3d20bfbbbf0ab4c184820c1711ea966460c
Владислав Кидун <vladkisun5@gmail.com>
2026-01-19 20:08:45 +0200

    Fix advert seeder. Change some names

--- WebApi/WebApi.BLL/MapperProfiles/AdvertisementMapperProfile.cs
+++ WebApi/WebApi.BLL/MapperProfiles/AdvertisementMapperProfile.cs
@@ -1,5 +1,5 @@
 ﻿using AutoMapper;
-using WebApi.BLL.DTOs.advertisement;
+using WebApi.BLL.DTOs.Advertisement;
 using WebApi.DAL.Entities;
 
 namespace WebApi.BLL.MapperProfiles;
@@ -18,12 +18,13 @@ public class AdvertisementMapperProfile : Profile
             .ForMember(dest => dest.Categories, opt => opt.Ignore());
 
         //AdvertisementEntity -> AdvertisementDTO
-        CreateMap<AdvertisementEntity, advertisementDTO>()
+        CreateMap<AdvertisementEntity, AdvertisementDTO>()
             .ForMember(dest => dest.Categories, opt => opt.MapFrom(src => src.Categories.Select(c => c.Name)))
             .ForMember(dest => dest.Images, opt => opt.MapFrom(src => src.Images.Select(i => i.ImageUrl)));
 
         CreateMap<SeederAdvertisementDTO, AdvertisementEntity>()
             .ForMember(dest => dest.Images, opt => opt.Ignore())
+            .ForMember(dest => dest.User, opt => opt.Ignore())
             .ForMember(dest => dest.Categories, opt => opt.Ignore());
     }
 }
